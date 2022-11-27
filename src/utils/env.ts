9a303cb10693f192571c13e4f7add76f5ce860ea
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-11-27 14:58:49 +0900

    fix env var

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,5 @@
-const getEnvironmentVariable = (envVar: string) => {
-  const dev = import.meta.env.DEV;
-  return dev ? import.meta.env[envVar] : process.env[envVar];
-};
+const dev = import.meta.env.DEV;
 
-export const GITHUB_API_KEY = getEnvironmentVariable("GITHUB_API_KEY");
+export const GITHUB_API_KEY = dev
+  ? import.meta.env.GITHUB_API_KEY
+  : process.env.GITHUB_API_KEY;
