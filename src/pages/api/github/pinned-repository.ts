0a681d087e45da4c9f7b19774ae3167f02d01ff8
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-11-27 12:35:00 +0900

    log output

--- src/pages/api/github/pinned-repository.ts
+++ src/pages/api/github/pinned-repository.ts
@@ -4,6 +4,7 @@ import { getPinnedRepositories } from "../../../utils/github";
 export const get: APIRoute = async () => {
   try {
     const repositories = await getPinnedRepositories();
+    console.log(repositories)
     return new Response(JSON.stringify(repositories), {
         headers: {
             "Cache-Control": `public,max-age=${10}`
