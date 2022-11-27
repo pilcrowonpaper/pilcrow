9dd9eb9462f79ae30702ba2676a69521bd70f029
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-11-27 13:04:00 +0900

    return body

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,7 +1,9 @@
 import type { APIRoute } from "astro";
 
 export const get: APIRoute = () => {
-    return new Response(JSON.stringify({
-        message: "hello"
-    }))
+    return {
+        body: JSON.stringify({
+            message: "hello"
+        })
+    }
 }
