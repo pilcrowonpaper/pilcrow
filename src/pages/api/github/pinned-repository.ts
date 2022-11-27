abff13a19c29c9833f5dc6d9c6ec9d7c234ce512
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2022-11-27 16:26:53 +0900

    increase cache time

--- src/pages/api/github/pinned-repository.ts
+++ src/pages/api/github/pinned-repository.ts
@@ -1,13 +1,14 @@
 import type { APIRoute } from "astro";
 import { getPinnedRepositories } from "../../../utils/github";
 
-export const get: APIRoute = async ({url}) => {
-  console.log(url.href)
+const CACHE_MAX_AGE = 60 * 60 * 24 // 24 hours
+
+export const get: APIRoute = async () => {
   try {
     const repositories = await getPinnedRepositories();
     return new Response(JSON.stringify(repositories), {
       headers: new Headers({
-        "Cache-Control": `public, max-age=${10}`,
+        "Cache-Control": `public, max-age=${CACHE_MAX_AGE}`,
         "Content-Type": "application/json",
       }),
     });
