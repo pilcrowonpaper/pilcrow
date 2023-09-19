75e4331bb4f9d9e01fc8f84216b7d15b5b0b0f31
lucythecat <pilcrowonpaper@gmail.com>
2023-09-19 21:18:20 +0900

    fix deps

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import markdown from "./integrations/markdown";
-import siena from "siena";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
