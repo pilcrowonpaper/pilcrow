01ce9fb6c1e63127d88a130826dd6f513fc0b8cd
lucythecat <pilcrowonpaper@gmail.com>
2023-11-12 23:22:12 +0900

    update colors

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,25 +6,31 @@ export const onRequest: MiddlewareResponseHandler = async (_, next) => {
 	const modifiedHtml = html
 		.replaceAll("background-color: #1E1E1E", "")
 		.replaceAll("#569CD6", COLORS.KEYWORD)
-		.replaceAll("#4FC1FF", COLORS.BLACK)
+		.replaceAll("#4FC1FF", COLORS.BASE)
 		.replaceAll("#C586C0", COLORS.KEYWORD)
-		.replaceAll("#9CDCFE", COLORS.BLACK)
+		.replaceAll("#9CDCFE", COLORS.BASE)
 		.replaceAll("#6A9955", COLORS.COMMENT)
 		.replaceAll("#DCDCAA", COLORS.FUNCTION)
 		.replaceAll("#4EC9B0", COLORS.KEYWORD)
 		.replaceAll("#CE9178", COLORS.STRING)
 		.replaceAll("#B5CEA8", COLORS.NUMBER)
-		.replaceAll("#D4D4D4", COLORS.BLACK);
+		.replaceAll("#D4D4D4", COLORS.BASE);
 	return new Response(modifiedHtml, {
 		headers: response.headers
 	});
 };
 
+const BLACK = "#000000";
+const PINK = "#f255a1";
+const BLUE = "#3685f5";
+const ORANGE = "#fc9c26";
+const GRAY = "#8D8D8D";
+
 const COLORS = {
-	COMMENT: "#8D8D8D",
-	BLACK: "#000000",
-	STRING: "#E28A17",
-	NUMBER: "#E92F89",
-	KEYWORD: "#518DFD",
-	FUNCTION: "#518DFD"
+	COMMENT: GRAY,
+	BASE: BLACK,
+	STRING: ORANGE,
+	NUMBER: PINK,
+	KEYWORD: BLUE,
+	FUNCTION: BLUE
 };
