4535f376df99838de0b19ef344caf790e1f64058
pilcrow <pilcrowonpaper@gmail.com>
2024-03-08 00:03:56 +0900

    Update rss.xml.ts

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -6,9 +6,7 @@ export async function GET() {
 
 	return rss({
 		title: "Pilcrow",
-		description:
-			"I think I'm best \"known\" for my work on auth libraries, but I'm interested in anything web dev... well maybe except CSS. I enjoy taking photos, drawing, playing games, traveling, and cooking. Italian food is my favorite.",
-		site: "https://pilcrowonpaper.com/",
+		description: "Pilcrow's personal website",
 		items: posts.map((post) => ({
 			title: post.metaData.title,
 			pubDate: post.metaData.date,
