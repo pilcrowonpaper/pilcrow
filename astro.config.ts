9b17114e191cbe658409c7c842577d9f57f78146
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-03-09 22:34:23 +0900

    update

--- astro.config.ts
+++ astro.config.ts
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), markdown()],
-	site: "https://pilcrowonpaper.com/"
+	site: "https://pilcrowonpaper.com"
 });
