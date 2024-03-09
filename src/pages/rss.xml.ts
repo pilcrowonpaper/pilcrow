b39163bac922866c1e49844731889ef4f4103251
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2024-03-09 22:34:05 +0900

    fix types

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,7 +1,9 @@
 import rss from "@astrojs/rss";
 import { getPosts } from "@utils/posts";
 
-export async function GET() {
+import type { APIContext } from "astro";
+
+export async function GET(context: APIContext) {
 	const posts = await getPosts();
 
 	return rss({
@@ -12,6 +14,7 @@ export async function GET() {
 			pubDate: post.metaData.date,
 			description: post.metaData.description,
 			link: post.href
-		}))
+		})),
+		site: context.site!
 	});
 }
