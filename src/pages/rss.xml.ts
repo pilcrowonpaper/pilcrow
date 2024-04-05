30bcb7ae69807de104fdeca8410ac2df71078963
skdishansachin <skdishansachin@gmail.com>
2024-04-05 10:58:25 +0530

    Update with the astro collection

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,21 @@
+import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { getPosts } from "@utils/posts";
 
 import type { APIContext } from "astro";
 
 export async function GET(context: APIContext) {
-	const posts = await getPosts();
+	const posts = await getCollection("posts", ({ data }) => {
+		return data.hidden != true;
+	});
 
 	return rss({
 		title: "Pilcrow",
 		description: "Pilcrow's personal website",
 		items: posts.map((post) => ({
-			title: post.metaData.title,
-			pubDate: post.metaData.date,
-			description: post.metaData.description,
-			link: post.href
+			title: post.data.title,
+			pubDate: post.data.date,
+			description: post.data.description,
+			link: `/blog/${post.id}`
 		})),
 		site: context.site!
 	});
